73caa110dbe6408698a06668315a2322f1b9d1c0
William Bishop <williambishop@Williams-MacBook-Pro.local>
2022-03-09 11:17:11 +0100

    basic drag drop functionality

--- src/app.js
+++ src/app.js
@@ -1,9 +1,45 @@
-async function init() {
-  const stickyNote = await miro.board.createStickyNote({
-    content: 'Hello, World!',
-  });
+// NOTE: app.js contains the the app logic for this starter app.
 
-  await miro.board.viewport.zoomTo(stickyNote);
+// ---> FUNCTION TO CREATE A STICKY NOTE AND ZOOM TO IT ON SDK LOAD:
+
+// async function init() {
+//   const stickyNote = await miro.board.createStickyNote({
+//     content: 'hello, puppets!',
+//   });
+
+//   const shape = await miro.board.createShape({
+//     content: '<p>This is a very yellow star shape.</p>',
+//     shape: 'star',
+//     style: {
+//       fillColor: '#FEFF45',
+//     },
+//     x: 200,
+//     y: 200,
+//     width: 280,
+//     height: 280,
+//   });
+
+//   await miro.board.viewport.zoomTo(stickyNote);
+//   //await miro.board.viewport.zoomTo(shape);
+// }
+
+init();
+
+// ---> FUNCTION TO ENABLE DRAG/DROP OF AN IMAGE FROM OPEN PANEL AFTER SDK IS LOADED:
+
+async function init() {
+  
+  // Enable the 'drop' event on the app panel. Active on 'miro-draggable' HTML elements
+  await miro.board.ui.on('drop', async ({x, y, target}) => {
+    // In this example: when the HTML element is dropped on the board, a sticky note is created
+    await miro.board.createImage({
+      x,
+      y,
+      url: target.src
+      // content: target.textContent,
+    });
+  });
 }
 
 init();
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+// NOTE: index.js contains the specific Miro Web SDK methods that you'd like to call 
+
 async function init() {
   miro.board.ui.on('icon:click', async () => {
     await miro.board.ui.openPanel({url: 'app.html'});
   });
 }
-
+//   miro.board.ui.openPanel({
+//     url: 'https://bishwm.me',
+//     height: 600,
+// });
 init();
