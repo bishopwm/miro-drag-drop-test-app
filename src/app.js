ab9cabe4d2339781ffcb937b40f2d879802b47fd
William Bishop <bishopwm@g.cofc.edu>
2022-03-25 16:02:30 +0100

    added comments

--- src/app.js
+++ src/app.js
@@ -1,7 +1,3 @@
-// NOTE: app.js contains the the app logic for this starter app.
-
-// ---> FUNCTION TO (1) ENABLE DRAG/DROP OF AN IMAGE FROM OPEN PANEL AFTER SDK IS LOADED, (2) LOAD PLACHOLDERS TO DRAG ITEMS TO
-
 async function init() {
   
   // Enable the 'drop' event on the app panel. Active on 'miro-draggable' HTML elements
